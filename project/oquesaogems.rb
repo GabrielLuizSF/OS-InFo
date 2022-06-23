6a4d70fe41699f26ed37fd6ba7d5ac600519405d
GabrielLuizSF <gabrielluizdeveloper7@gmail.com>
2022-06-23 12:18:03 -0300

    o que sao gems

---
+++ project/oquesaogems.rb
@@ -0,0 +1,20 @@
+# O QUE SÃO GEMS ?
+
+
+#gems são pacotes(também conhecidos como libs ou bibliotecas da linguagem rub) que podem ser instalados para facilitar o desenvolvimento
+
+#gem list=morstra todas as gems do seu pc | | gem uninstall nomedagem = desinstala a gem || gem i nomedagem = instala a gem
+
+#para instalar todas as gems de um projeto invés de uma gem de cada vez utiliza-se o bundler
+
+#primeiro crie um arquivo chamado Gemfile
+
+#coloque o seguinte  código dentro do Gemfile:
+
+source "https://rubygems.org"
+
+gem 'nomedagem'
+gem 'nomedagem'
+
+# use no terminal gem i bundler depois use o comando bundler no terminal e todas as gems do gemfile vão ser instaladas
+
